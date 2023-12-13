28044a6a71e1395bb5186b0074b6712aa7a71afa
Caleb Schwarze <ces55739@bethel.edu>
2023-12-13 14:43:46 -0600

    slightly updated part 2 to have it set

--- year2023/day5/__init__.py
+++ year2023/day5/__init__.py
@@ -14,7 +14,7 @@ def run():
     print("--- PART 1 ---")
     print("Lowest location num ")
     print(part1(lines))
-    assert part1(lines) == 525792406
+    assert part1(lines) == 35
     print("--- PART 2 ---")
     print("")
     print(part2(lines))
@@ -39,7 +39,7 @@ def part1(lines):
                         'lines': [],
                     }
             else:
-                almanac[map_from]['lines'].append(x.split(' '))
+                almanac[map_from]['lines'].append(line.split(' '))
 
     lowest_seed_value = -1
     for seed in almanac['seeds']:
@@ -67,35 +67,50 @@ def part1(lines):
 def part2(lines):
     almanac = {}
     map_from = ''
+
+    # This should build out our process
     for line in lines:
         if len(line) != 0:
             if line[0].isalpha():
                 if 'seeds: ' in line:
-                    almanac['seeds'] = line.split(': ')[1].split(' ')
+                    almanac['seeds'] = []
+                    # use [::2]
+                    range_start = line.split(': ')[1].split(' ')[::2]
+                    range_end = line.split(': ')[1].split(' ')[1::2]
+                    for index in range(len(range_start)):
+                        almanac['seeds'].append({
+                            'range-start': range_start[index],
+                            'range-end': range_end[index],
+                        })
                 else:
                     map_from = line.split('-to-')[0]
                     map_to = line.split('-to-')[1].replace(' map:', '')
                     almanac[map_from] = {
                         'from': map_from,
                         'to': map_to,
-                        'values': {},
-                        'lines': [],
+                        'ranges': []
                     }
             else:
-                almanac[map_from]['lines'].append(x.split(' '))
+                # TODO - this needs the values set properly
+                almanac[map_from]['ranges'].append(line.split(' '))
 
-    lowest_seed_value = -1
+    # TODO - work backwards, since we have a "limited" data set at the end
+    lowest_seed_value = 1
+    for lowest_seed_value in range(1,100000):
 
-    for index, seed_range in enumerate(almanac['seeds'][::2]):
-        start = int(seed_range)
-        end = int(seed_range) + int(almanac['seeds'][(index*2)+1])
+        from_directory = 'location'
+        start_value = lowest_seed_value
 
-        # todo - from here we need to connect the ranges together.
-        #      - Range1 -> range2, but also due to overlap, Range1 -> range2b, etc.
-        #      - With that mapping, then we need to find the lowest range.
+        while from_directory != 'location':
+            directory = almanac[from_directory]
+            destination = directory['to']
 
+            for specified_range in directory['lines']:
+                if int(specified_range[1]) <= start_value <= int(specified_range[1]) + int(specified_range[2]):
+                    start_value = start_value - int(specified_range[1]) + int(specified_range[0])
+                    break
 
-        # TODO - a 2nd idea would be to try to work backwards, since we have a "limited" data set at the end
-
+            # at the end, move on!
+            from_directory = destination
 
     return lowest_seed_value
